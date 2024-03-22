7ff64058f1b2c60b78a8d8c634673ce101409c4a
hasnain099 <86136628+hasnain099@users.noreply.github.com>
2024-03-22 21:27:36 +0500

    Add files via upload

---
+++ Remove_Comma_At_last.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int  arr[4] = {1 ,2 ,4,6};
+    for(int i = 0 ; i<=3 ;i++){
+         cout<<arr[i];
+         if(arr[i] == arr[3])
+             return 0;
+     for(int j = 0 ; j<1 ;j++){
+         cout<<" , ";
+         
+         }
+    }
+}
